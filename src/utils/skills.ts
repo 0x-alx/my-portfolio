268db0bac77220fd6d56aa273868c11c3dbc5e3e
0x-alx <alx.charbo@gmail.com>
2023-09-20 15:39:26 +0200

    NEW - json data

---
+++ src/utils/skills.ts
@@ -0,0 +1,68 @@
+import * as Icon from "../assets/images";
+
+export const skillsJson = [
+	{
+		name: "Typescript",
+		icon: Icon.typescriptIcon
+	},
+	{
+		name: "Javascript",
+		icon: Icon.javascriptIcon,
+	},
+	{
+		name: "React",
+		icon: Icon.reactIcon,
+	},
+	{
+		name: "NodeJS",
+		icon: Icon.nodeIcon,
+	},
+	{
+		name: "ExpressJS",
+		icon: Icon.expressIcon,
+	},
+	{
+		name: "Redux",
+		icon: Icon.reduxIcon,
+	},
+	{
+		name: "ViteJS",
+		icon: Icon.viteIcon,
+	},
+	{
+		name: "HTML",
+		icon: Icon.htmlIcon,
+	},
+	{
+		name: "CSS",
+		icon: Icon.cssIcon,
+	},
+	{
+		name: "TailwindCSS",
+		icon: Icon.tailwindIcon,
+	},
+	{
+		name: "MaterialUI",
+		icon: Icon.muiIcon,
+	},
+	{
+		name: "Figma",
+		icon: Icon.figmaIcon,
+	},
+	{
+		name: "MongoDB",
+		icon: Icon.mongodbIcon,
+	},
+	{
+		name: "Storybook",
+		icon: Icon.storybookIcon,
+	},
+	{
+		"name": "Netlify",
+		"icon": Icon.netlifyIcon
+	},
+	{
+		"name": "Postman",
+		"icon": Icon.postmanIcon
+	}
+];
