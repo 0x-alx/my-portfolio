8b68dbf0d9d41b1d8322b075689154b5228ab35d
0x-alx <alx.charbo@gmail.com>
2023-09-20 15:42:48 +0200

    UPDATE - Homepage with new components

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -1,5 +1,21 @@
+import BottomBar from "../components/BottomBar/BottomBar";
+import Launchpad from "../components/Launchpad/Launchpad";
+import Spotify from "../components/Spotify/Spotify";
+import Terminal from "../components/Terminal/Terminal";
+import TopBar from "../components/TopBar/TopBar";
+
 const Homepage = () => {
-	return <div>Homepage</div>;
+	return (
+		<div className='h-screen bg-mainBackground bg-cover relative'>
+			<TopBar />
+			<Spotify />
+			<Launchpad />
+			{/* <Terminal /> */}
+			<div className='flex justify-center items-center fixed bottom-5 w-full'>
+				<BottomBar />
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
