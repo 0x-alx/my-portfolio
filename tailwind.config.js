57deb084a165e5e3e6d7fbc50dcaf1e88b5f9dd0
0x-alx <alx.charbo@gmail.com>
2023-09-21 13:32:10 +0200

    NEW - DaisyUI added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/flowbite/**/*.js",
+	],
 	theme: {
+		backgroundImage: {
+			mainBackground: "url('/src/assets/images/background.jpg')",
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
