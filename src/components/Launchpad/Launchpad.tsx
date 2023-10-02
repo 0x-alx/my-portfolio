022a33097746c47a17194cd8a2519092c8fe7a55
0x-alx <alx.charbo@gmail.com>
2023-10-02 15:15:32 +0200

    UPDATE - scrollable Launchpad view

--- src/components/Launchpad/Launchpad.tsx
+++ src/components/Launchpad/Launchpad.tsx
@@ -3,11 +3,11 @@ import { skillsJson } from "../../utils/skills.js";
 
 const Launchpad = () => {
 	return (
-		<div className='w-full h-full absolute z-2 bg-[rgba(31,41,55,0.25)] backdrop-blur-md px-[200px] py-[80px] '>
+		<div className='w-full h-full m-h-screen absolute z-2 bg-[rgba(31,41,55,0.25)] backdrop-blur-md px-[200px] py-[80px] overflow-scroll'>
 			<h1 className='flex justify-center text-white font-bold text-2xl mb-6'>
 				Technical skills
 			</h1>
-			<ul className='flex flex-row gap-x-[80px] gap-y-[40px] flex-wrap'>
+			<ul className='flex flex-row gap-x-[80px] gap-y-[40px] flex-wrap justify-center'>
 				{skillsJson.map((skill) => {
 					return (
 						<LaunchpadApp
