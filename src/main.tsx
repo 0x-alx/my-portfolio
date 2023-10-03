d5177cf5529e10d92f09f7377ea16d87388d4ec9
0x-alx <alx.charbo@gmail.com>
2023-10-03 13:32:25 +0200

    NEW - Router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Homepage } from "./views";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import Login from "./views/Login";
 
-// import Login from "./views/Login";
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Login />,
+	},
+	{
+		path: "/home",
+		element: <Homepage />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<Homepage />
-			{/* <Login /> */}
+			<RouterProvider router={router} />
 		</Provider>
 	</React.StrictMode>
 );
