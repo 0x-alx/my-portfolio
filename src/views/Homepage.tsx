23a476d1d4c6bfedc6ba9275f471e5e82586f93f
0x-alx <alx.charbo@gmail.com>
2023-10-03 13:35:43 +0200

    NEW - Fake login ok

--- src/views/Homepage.tsx
+++ src/views/Homepage.tsx
@@ -5,12 +5,19 @@ import Terminal from "../components/Terminal/Terminal";
 import TopBar from "../components/TopBar/TopBar";
 import Notes from "../components/Notes/Notes";
 import { useAppSelector } from "../app/hooks";
+import { useEffect } from "react";
 
 const Homepage = () => {
 	const terminalVisible = useAppSelector((state) => state.terminal.value);
 	const launchpadVisible = useAppSelector((state) => state.launchpad.value);
 	const notesVisible = useAppSelector((state) => state.notes.value);
-	console.log(terminalVisible);
+
+	useEffect(() => {
+		if (sessionStorage.getItem("logged_in") !== "true") {
+			window.location.href = "/";
+		}
+	});
+
 	return (
 		<div className='h-screen bg-mainBackground bg-cover relative'>
 			<TopBar />
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,12 +1,23 @@
+// import "dotenv/config";
 import { Avatar, Input } from "react-daisyui";
 import avatar from "../assets/images/avatar.jpg";
 import TopBar from "../components/TopBar/TopBar";
+import { useState } from "react";
 
 const Login = () => {
+	const [password, setPassword] = useState("");
+
+	const handleSubmit = () => {
+		if (password === "hire_me123") {
+			window.location.href = "/home";
+			sessionStorage.setItem("logged_in", "true");
+		} else console.log("Wrong password");
+	};
+
 	return (
 		<div className='h-screen bg-mainBackground bg-cover relative'>
 			<TopBar transparent />
-			<div className='flex flex-col w-full max-w-[250px] items-center justify-center absolute top-[280px] left-1/2 translate-x-[-50%] gap-4'>
+			<div className='flex flex-col w-full max-w-[250px] items-center justify-center absolute top-[28%] left-1/2 translate-x-[-50%] gap-4'>
 				<Avatar
 					src={avatar}
 					shape='circle'
@@ -19,6 +30,14 @@ const Login = () => {
 					className='h-[30px] bg-[rgba(255,255,255,0.25)] backdrop-blur-md rounded-[20px] text-white max-w-[180px]'
 					placeholder='Enter password'
 					type='password'
+					onChange={(e) => {
+						setPassword(e.target.value);
+					}}
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							handleSubmit();
+						}
+					}}
 				/>
 			</div>
 		</div>
