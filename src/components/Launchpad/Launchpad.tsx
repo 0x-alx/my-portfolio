343a2c50dc8e7f5fefa1be9a70e8b5e73a1e4990
0x-alx <alx.charbo@gmail.com>
2023-10-06 10:20:42 +0200

    UPDATE - remove pointer clic - close on click outside

--- src/components/Launchpad/Launchpad.tsx
+++ src/components/Launchpad/Launchpad.tsx
@@ -1,9 +1,17 @@
 import LaunchpadApp from "./LaunchpadApp";
 import { skillsJson } from "../../utils/skills.js";
+import { useAppDispatch } from "../../app/hooks.js";
+import { close as closeLaunchpad } from "../Launchpad/launchpadSlice";
 
 const Launchpad = () => {
+	const dispatch = useAppDispatch();
 	return (
-		<div className='w-full h-full m-h-screen absolute z-2 bg-[rgba(31,41,55,0.25)] backdrop-blur-md md:px-[200px] py-[80px] overflow-scroll'>
+		<div
+			className='w-full h-full m-h-screen absolute z-2 bg-[rgba(31,41,55,0.25)] backdrop-blur-md md:px-[200px] py-[80px] overflow-scroll'
+			onClick={() => {
+				dispatch(closeLaunchpad());
+			}}
+		>
 			<h1 className='flex justify-center text-white font-bold text-2xl mb-6'>
 				Technical skills
 			</h1>
